a8ca4381859e67b2c8986b59c4bc5362f8e24ef8
Liliana <lilianakoric@gmail.com>
2024-09-06 20:56:44 -0700

    kata7,8 was added with new vide range of methods

--- return_statement_is: "Who ate.py
+++ return_statement_is: "Who ate.py
@@ -6,9 +6,17 @@ def cookie(x):
     else:
         return "Who ate the last cookie? It was the dog!"
 
-def cookie(x):
-    return "Who ate the last cookie? It was %s!" % {str:"Zach", float:"Monica", int:"Monica"}.get(type(x), "the dog")
 
-def cookie(x):
-  return f'Who ate the last cookie? It was {"Zach" if type(x) is str else "Monica" if type(x) in [int, float] else "the dog"}!'
+def cookie1(x):
+    return "Who ate the last cookie? It was %s!" % {str: "Zach", float: "Monica", int: "Monica"}.get(type(x), "the dog")
 
+
+def cookie2(x):
+    return f'Who ate the last cookie? It was {"Zach" if type(x) is str else "Monica" if type(x) in [int, float] else "the dog"}!'
+
+
+def greet(name):
+    if name == "Johnny":
+        return "Hello, my love!"
+    else:
+        return "Hello, {name}!".format(name=name)
