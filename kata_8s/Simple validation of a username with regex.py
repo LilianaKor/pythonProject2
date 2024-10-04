13d7e985b86ccfe921d058ab46766a1b287a8213
Liliana <lilianakoric@gmail.com>
2024-10-04 13:06:33 -0700

    kata-8 was improved with new vide range of methods

--- kata_8s/Simple validation of a username with regex.py
+++ kata_8s/Simple validation of a username with regex.py
@@ -1,4 +1,6 @@
 import re
+
+
 def validate_usr(username):
     print(username)
     pattern = r'[a-z0-9_]{4,16}'
@@ -6,26 +8,28 @@ def validate_usr(username):
     print(x)
     return bool(x)
 
-import re
-def validate_usr(un):
+
+def validate_usr1(un):
     return re.match('^[a-z0-9_]{4,16}$', un) != None
 
+
 from re import fullmatch
-def validate_usr(username):
+
+
+def validate_usr2(username):
     return bool(fullmatch(r"[_0-9a-z]{4,16}", username))
+
 
 import re
 valid_username = re.compile(r"[a-z0-9_]{4,16}")
-def validate_usr(username):
+
+
+def validate_usr3(username):
     return bool(re.fullmatch(valid_username, username))
-
-
 
 # DESCRIPTION:
 # Write a simple regex to validate a username. Allowed characters are:
-#
 # lowercase letters,
 # numbers,
 # underscore
 # Length should be between 4 and 16 characters (both included).
-#
--- kata_8s/Stringy Strings.py
+++ kata_8s/Stringy Strings.py
@@ -1,9 +1,10 @@
 def stringy(size):
     return ''.join('1' if i % 2 == 0 else '0' for i in range(size))
 
+
 # Examples
-print(stringy(6))   # Output: '101010'
-print(stringy(4))   # Output: '1010'
+print(stringy(6))  # Output: '101010'
+print(stringy(4))  # Output: '1010'
 print(stringy(12))  # Output: '101010101010'
 
 
@@ -19,6 +20,7 @@ def stringy(size):
     for x in range(0, size):
         res += "1" if x % 2 == 0 else "0"
     return res
+
 
 stringy = lambda size: ('10' * size)[:size]
 
