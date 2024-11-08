a2d9ad113d9a2703a55a4c60dab2ddbd0574c960
Liliana <lilianakoric@gmail.com>
2024-11-07 17:26:10 -0800

    reform.kata-7 convert_mpg_to_kmpl

--- kata_7/All Star Code Challenge #3.py
+++ kata_7/All Star Code Challenge #3.py
@@ -8,26 +8,32 @@ def remove_vowels(strng):
             str += i
     return str
 
-def remove_vowels(stg):
+
+def remove_vowels1(stg):
     return "".join(c for c in stg if c not in "aeiou")
 
-def remove_vowels( _s ):
-    return _s.replace('a','').replace('e','').replace('i','').replace('o','').replace('u','')
 
-def remove_vowels(strng):
-    str = ['a','i','u','e','o']
+def remove_vowels2(_s):
+    return _s.replace('a', '').replace('e', '').replace('i', '').replace('o', '').replace('u', '')
+
+
+def remove_vowels3(strng):
+    str = ['a', 'i', 'u', 'e', 'o']
     for i in str:
         strng = strng.replace(i, "")
     return strng
 
-def remove_vowels(s):
+
+def remove_vowels4(s):
     res = ""
     for char in s:
         if char.lower() not in "aeiou":
             res += char
     return res
 
-remove_vowels = lambda s: __import__("re").sub(r"[aeiou]", "", s)
 
-def remove_vowels(s):
-    return ''.join(i for i in s if i not in'aeiou')
+remove_vowels5 = lambda s: __import__("re").sub(r"[aeiou]", "", s)
+
+
+def remove_vowels6(s):
+    return ''.join(i for i in s if i not in 'aeiou')
