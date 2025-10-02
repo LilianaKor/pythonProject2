94e615d8b97dc8118f740507cdb7cdeffdb13eb9
Liliana <lilianakoric@gmail.com>
2025-10-01 21:33:10 -0700

    Added  blank_pages_need.py RF

--- blank_pages_need.py
+++ blank_pages_need.py
@@ -1,22 +1,25 @@
 def paperwork(n, m):
-    if (n<0 or m<0):
+    if (n < 0 or m < 0):
         return 0
     else:
-        total = n*m
+        total = n * m
         return total
 
-def paperwork(n, m):
+
+def paperwork1(n, m):
     return n * m if n > 0 and m > 0 else 0
 
-def paperwork(n, m):
-        if m < 0 or n < 0:
-            return 0
-        else:
-            return n * m
 
-def paperwork(n, m):
+def paperwork2(n, m):
+    if m < 0 or n < 0:
+        return 0
+    else:
+        return n * m
+
+
+def paperwork3(n, m):
     return max(n, 0) * max(m, 0)
 
-def paperwork(n, m):
-    return 0 if n < 0 or m < 0 else n*m
 
+def paperwork(n, m):
+    return 0 if n < 0 or m < 0 else n * m
