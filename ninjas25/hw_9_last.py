e55ec4c127f2f0b09a070a77aac4c3dc23774bfa
Liliana <lilianakoric@gmail.com>
2025-10-18 17:08:13 -0700

    Added class RegistrationError(Exception)

---
+++ ninjas25/hw_9_last.py
@@ -0,0 +1,31 @@
+"""
+Регистрация (часть 1)
+
+1. Напишите функцию registration(), которая принимает 2 аргумента -
+username (строка) и password() (строка)
+2. Функция должна проверить username, а именно:
+a. username является строкой.
+b. Длина этой строки минимум 4 символа и максимум 15.
+c. username состоит только из букв.
+3. Если username некорректный, то функция должна вызвать исключение
+ValueError.
+4. Функция должна проверить password, а именно:
+a. password является строкой.
+b. Длина этой строки минимум 8 символов и максимум 45.
+c. password состоит только из букв и цифр.
+5. Если password некорректный, то функция должна вызвать исключение
+ValueError.
+6. Если “регистрация” прошла успешно, то функция должна вернуть True.
+
+Дополнительно: создайте свой тип исключения RegistrationError и используйте
+его вместо ValueError.
+"""
+
+
+class RegistrationError(Exception):
+    pass
+
+
+def registration(username, password):
+    if not (isinstance(username, str) and 4 <= len(username) <= 15 and username.isalpha()):
+        raise RegistrationError('username incorrect')
